4a447288f9ee58a4feaf65f1798447058c29e435
theingmwe <theingmweoo.tmo@gmail.com>
2023-03-21 16:42:47 -0700

    added new stuff to MovieList.kt

--- app/src/main/java/com/example/eivom/data/MovieList.kt
+++ app/src/main/java/com/example/eivom/data/MovieList.kt
@@ -5,5 +5,10 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class MovieList(
     val title: String,
-    val release_date: String
+    val overview: String,
+    val poster_path: String,
+    val release_date: String,
+    val genre: List<Int>
 )
+
+
