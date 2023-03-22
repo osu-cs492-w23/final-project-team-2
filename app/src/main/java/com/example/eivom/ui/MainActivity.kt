9ad76c22b6922d351801ebcf774e4b8249772fa6
theingmwe <theingmweoo.tmo@gmail.com>
2023-03-21 19:25:15 -0700

    trying to push

--- app/src/main/java/com/example/eivom/ui/MainActivity.kt
+++ app/src/main/java/com/example/eivom/ui/MainActivity.kt
@@ -2,32 +2,43 @@ package com.example.eivom.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.eivom.R
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.navigateUp
+import com.example.eivom.data.MovieList
 
 
 const val MOVIEDATABASE_APPID = "9b548beeca2515183884381852406153"
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
+    private val movieInfoAdapter = MovieInfoAdapter(::onMoviePosterClick)
+
     private lateinit var appBarConfig : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.movie_info_test)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_fragment
-        )as NavHostFragment
+//        val navHostFragment = supportFragmentManager.findFragmentById(
+//            R.id.nav_host_fragment
+//        )as NavHostFragment
 
-        val navController = navHostFragment.navController
+//        val navController = navHostFragment.navController
 
-        appBarConfig = AppBarConfiguration(navController.graph)
+//        appBarConfig = AppBarConfiguration(navController.graph)
+//
+//        setupActionBarWithNavController(navController, appBarConfig)
+    }
 
-        setupActionBarWithNavController(navController, appBarConfig)
+    private fun onMoviePosterClick(detail: MovieList) {
+        Log.d(TAG, "Movie poster clicked")
     }
 
     override fun onSupportNavigateUp(): Boolean{
