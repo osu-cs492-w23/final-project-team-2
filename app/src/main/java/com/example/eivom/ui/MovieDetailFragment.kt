34440373fd5dbbc23d1ac029f68243336991038f
NicksonEdbert <nicksonedbert@gmail.com>
2023-03-22 20:21:02 -0700

    pass data into movie detail

--- app/src/main/java/com/example/eivom/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/example/eivom/ui/MovieDetailFragment.kt
@@ -1,15 +1,20 @@
 package com.example.eivom.ui
 
+import android.media.Image
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.eivom.R
 import org.w3c.dom.Text
 
 class MovieDetailFragment : Fragment(R.layout.movie_details_activity) {
+    private val args: MovieDetailFragmentArgs by navArgs()
 
     override fun onViewCreated(
         view: View,
@@ -17,9 +22,13 @@ class MovieDetailFragment : Fragment(R.layout.movie_details_activity) {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-    }
+        view.findViewById<TextView>(R.id.movieTitle).text = getString(R.string.movie_title, args.moviedetail.title)
 
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//    }
+        Glide.with(this)
+            .load(args.moviedetail.poster_path)
+            .into(view.findViewById(R.id.moviePoster))
+
+        view.findViewById<TextView>(R.id.movieDescription).text = getString(R.string.movie_description, args.moviedetail.overview)
+
+    }
 }
